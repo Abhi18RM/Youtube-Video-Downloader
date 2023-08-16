03d7cb2bbbe18e11bb9a589c9f3b9a92a5fa141b
Abhiram Ravuri <101940348+Abhi18RM@users.noreply.github.com>
2023-08-16 20:15:27 +0530

    updated error handling

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,11 @@ import fs from "fs";
 import ytdl from "ytdl-core";
 import ffmpeg from "fluent-ffmpeg";
 import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
-import path from "path";
+import path, { resolve } from "path";
 
 const app = express();
 app.use(express.static("public"));
-const baseDirectory = process.cwd(); 
+const baseDirectory = process.cwd();
 
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
@@ -15,79 +15,87 @@ app.get("/", async (req, res) => {
     res.status(200).send("Home Page");
 });
 
-app.get("/download/:filename", async(req,res)=>{
+app.get("/download/:filename", async (req, res) => {
     const fileName = req.params.filename + ".mp4";
-    const filePath = path.join(baseDirectory,"public",fileName);
-    if (fs.existsSync(filePath)){
-        res.download(filePath,fileName);
-    }else{
+    const filePath = path.join(baseDirectory, "public", fileName);
+    if (fs.existsSync(filePath)) {
+        res.download(filePath, fileName, async (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send("Error during download");
+            } else {
+                try {
+                    await fs.promises.unlink("public/" + fileName);
+                    console.log(`File ${fileName} is deleted`);
+                } catch (unlinkErr) {
+                    console.error("Error deleting file", unlinkErr);
+                }
+            }
+        });
+    } else {
         res.status(404).send("File not found");
     }
 });
 
 app.post("/", async (req, res) => {
     const videoURL = req.query.url;
-    const videoId = ytdl.getVideoID(videoURL);
-        // "https://www.youtube.com/watch?v=3WaxQMELSkw&list=PLgUwDviBIf0pwFf-BnpkXxs0Ra0eU2sJY&index=6";
-    try {
-        const paths = [];
-        const info = await ytdl.getBasicInfo(videoURL);
-        //getting the highest quaility ie 1080p
-        const video = ytdl.chooseFormat(info.formats, {quality: "137"});
-        //getting audio
-        const audio = ytdl.chooseFormat(info.formats, { quality: "140" });
-        //Parsing the format type
-        const mimeType = video.mimeType.split(";");
-        const mimePart1 = mimeType[0].split("/")[1];
+    if (ytdl.validateURL(videoURL)) {
+        const videoId = ytdl.getVideoID(videoURL);
+        try {
+            const info = await ytdl.getBasicInfo(videoURL);
+            //getting the highest quaility ie 1080p
+            const video = ytdl.chooseFormat(info.formats, { quality: "137" });
+            //getting audio
+            const audio = ytdl.chooseFormat(info.formats, { quality: "140" });
+            //Parsing the format type
+            const mimeType = video.mimeType.split(";");
+            const mimePart1 = mimeType[0].split("/")[1];
 
-        
+            // Download video with audio and pipe to the writable stream
+            const videoStream = ytdl(videoURL, { format: video });
+            const audioStream = ytdl(videoURL, { format: audio });
 
-        // Download video with audio and pipe to the writable stream
-        const videoStream = ytdl(videoURL, { format: video });
-        const audioStream = ytdl(videoURL, { format: audio });
+            const videoPath = `temp_${Date.now()}.mp4`;
+            const audioPath = `temp_${Date.now()}.aac`;
 
-        const videoPath = `temp_${Date.now()}.mp4`;
-        const audioPath = `temp_${Date.now()}.aac`;
-        paths.push(videoPath);
-        paths.push(audioPath);
+            videoStream.pipe(fs.createWriteStream(videoPath));
+            audioStream.pipe(fs.createWriteStream(audioPath));
 
-        videoStream.pipe(fs.createWriteStream(videoPath));
-        audioStream.pipe(fs.createWriteStream(audioPath));
+            console.log("video and audio files downloaded");
+            console.log("now about to merge");
 
-        await Promise.all([
-            new Promise((resolve) => videoStream.on("end", resolve)),
-            new Promise((resolve) => audioStream.on("end", resolve)),
-        ]);
+            await Promise.all([
+                new Promise((resolve) => videoStream.on("end", resolve)),
+                new Promise((resolve) => audioStream.on("end", resolve)),
+            ]);
 
-        const pub = "public";
-        const mergedPath = `${pub}/${videoId}.${mimePart1}`;
-        ffmpeg()
-            .input(videoPath)
-            .input(audioPath)
-            .outputOptions("-c:v", "copy")
-            .outputOptions("-c:a", "aac")
-            .outputOptions("-strict", "experimental")
-            .output(mergedPath)
-            .on("end", () => {
-                console.log("Merging complete");
-                paths.forEach((path)=>{
-                    fs.unlinkSync(path, (err)=>{
-                        if (err) {
-                            console.log(err);
-                        }else{
-                            console.log("File deleted successfully");
-                        }
-                    })
-                });
-                res.status(200).send({videoId});
-            })
-            .on("error", (err) => {
-                console.error("Error:", err);
-            })
-            .run();
+            const pub = "public";
+            const mergedPath = `${pub}/${videoId}.${mimePart1}`;
+            ffmpeg()
+                .input(videoPath)
+                .input(audioPath)
+                .outputOptions("-c:v", "copy")
+                .outputOptions("-c:a", "aac")
+                .outputOptions("-strict", "experimental")
+                .output(mergedPath)
+                .on("end", async () => {
+                    console.log("Merging complete");
+                    await Promise.all([
+                        new Promise((resolve) => fs.unlink(videoPath, resolve)),
+                        new Promise((resolve) => fs.unlink(audioPath, resolve)),
+                    ]);
+                    res.status(200).send({ videoId });
+                })
+                .on("error", (err) => {
+                    console.error("Error:", err);
+                })
+                .run();
 
-    } catch (err) {
-        res.status(500).send(err);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }else{
+        res.status(500).send("Not a valid URL");
     }
 });
 
